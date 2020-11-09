adc1643bb2f2c9cd93daee647aeb9137a67318ea
Tri Nguyen <tri.nguyen@naivecat.com>
2020-11-09 13:00:38 +0700

    Initialize project

---
+++ src/main/java/com/naivecat/closestprimenumber/ClosestPrimeNumberApplication.java
@@ -0,0 +1,13 @@
+package com.naivecat.closestprimenumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosestPrimeNumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClosestPrimeNumberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naivecat/closestprimenumber/ClosestPrimeNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naivecat.closestprimenumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClosestPrimeNumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
