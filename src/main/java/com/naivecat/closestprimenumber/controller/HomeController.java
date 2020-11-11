e0218bf606d5121b2ea9cc33752337c6f05d3b7e
Tri Nguyen <tri.nguyen@naivecat.com>
2020-11-11 15:12:15 +0700

    Refactor HomeController

--- src/main/java/com/naivecat/closestprimenumber/controller/HomeController.java
+++ src/main/java/com/naivecat/closestprimenumber/controller/HomeController.java
@@ -1,8 +1,6 @@
 package com.naivecat.closestprimenumber.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,4 +14,5 @@ public class HomeController {
         String result = "Hello World";
         return ResponseEntity.ok(result);
     }
+    
 }
